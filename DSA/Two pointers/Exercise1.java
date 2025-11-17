150a45d8656766c2e9656145ecd8d5853f9c0758
Andrey Villegas <timeofgetup@gmail.com>
2025-11-17 00:01:39 -0500

    Create Exercise1.java

---
+++ DSA/Two pointers/Exercise1.java
@@ -0,0 +1,46 @@
+package com.togudv;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = {1,2,3,4,5,6,7,65,73};
+
+        System.out.println("Ingrese el target de la suma");
+        int target = scanner.nextInt();
+
+        sumTarget(arr, target);
+    }
+
+
+    public static void sumTarget(int[] arr, int target ) {
+        int pointer1 = 0;
+        int pointer2 = arr.length - 1;
+
+        while(pointer1 < pointer2) {
+
+            int sum = arr[pointer1] + arr[pointer2];
+
+            if(target > sum ) {
+                pointer1++;
+            }
+
+            else if(target < sum) {
+                pointer2--;
+            }
+
+            else {
+                System.out.println(arr[pointer1]+", "+ arr[pointer2]);
+                return;
+            }
+
+
+        }
+
+        System.out.println("No se pudo lograr la suma target");
+
+    }
+}
